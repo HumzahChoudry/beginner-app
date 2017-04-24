05226b095f9dd3554fcad6e67865fa33576db638
Humzah Choudry <humzahchoudry@Humzahs-MacBook-Pro.local>
2017-04-24 14:22:47 -0500

    added files initially

--- Exercise1/ViewController.swift
+++ Exercise1/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HappyPhoto: UIImageView!
+    @IBOutlet weak var SadPhoto: UIImageView!
+    @IBOutlet weak var HappyButton: UIButton!
+    @IBOutlet weak var SadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideSad(_ sender: Any) {
+        SadPhoto.isHidden = true
+    }
 
+    @IBAction func HideHappy(_ sender: Any) {
+        HappyPhoto.isHidden = true
+    }
 }
 
